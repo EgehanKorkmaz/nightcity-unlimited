c40b1e38fa68271ed3dd738e2e1f26606709aaaa
Egehan Korkmaz <egehankrkmz@gmail.com>
2026-09-11 01:41:38 +0300

    mobile crush fix try 4

--- app/page.jsx
+++ app/page.jsx
@@ -58,7 +58,7 @@ export default function Home() {
             className="w-full min-h-screen"
           >
             <Hero />
-            <Categories />
+            {/*<Categories />*/}
             <About />
             <Contact />
           </motion.div>
